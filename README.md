567edb906d38b4c816f2381cbbb80ac55ad80927
sriram deevi <SRIDEEE@Mac.lan>
2026-07-05 22:28:14 -0500

    added readme and a gitignore for the virtual environment

---
+++ README.md
@@ -0,0 +1,6 @@
+This app runs all of the tools I want, in helping me with aerospace work at TAMU.
+
+This app is a GUI that will have multiple pages.
+- A grade calculator, that also calculates current GPA, an estimation GPA tracker, current grade tracker, and what I'll need on a final calculator.
+
+- Any other iterations are TBD and will be added on a need-to-need basis. 
--- app.py
+++ app.py
@@ -2,18 +2,17 @@ import sys
 from PyQt6.QtWidgets import QApplication, QWidget
 
 def main():
-    # Initialize the application
+    
     app = QApplication(sys.argv)
     
-    # Create a basic window widget
+    
     window = QWidget()
     window.resize(1600, 800)
     window.setWindowTitle('Flight Sim Test Window')
     
-    # Show the window on screen
+    
     window.show()
     
-    # Safely close out the application process when exiting
     sys.exit(app.exec())
 
 if __name__ == '__main__':
